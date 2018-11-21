fbd6054be2f8e7b3028656624e99ed89db9b05b6
Muruganandham K <muruganandham23@gmail.com>
2018-11-21 10:28:16 +0530

    Created Project

---
+++ OSControl/OSControl.h
@@ -0,0 +1,19 @@
+//
+//  OSControl.h
+//  OSControl
+//
+//  Created by Muruganandham.Kuppan on 21/11/18.
+//  Copyright © 2018 Muruganandham.Kuppan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSControl.
+FOUNDATION_EXPORT double OSControlVersionNumber;
+
+//! Project version string for OSControl.
+FOUNDATION_EXPORT const unsigned char OSControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSControl/PublicHeader.h>
+
+
---
+++ OSControlTests/OSControlTests.m
@@ -0,0 +1,37 @@
+//
+//  OSControlTests.m
+//  OSControlTests
+//
+//  Created by Muruganandham.Kuppan on 21/11/18.
+//  Copyright © 2018 Muruganandham.Kuppan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OSControlTests : XCTestCase
+
+@end
+
+@implementation OSControlTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
